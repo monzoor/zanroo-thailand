f96324007cdb7d762b69ffdef2ebd0a6a3cea48b
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2017-08-29 14:34:43 +0600

    init project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const name = 'tamal'
+    return (
+      <div>
+        <h1>My name is {name} </h1>
+        <i className="icon-ipay"></i>
+      </div>
+    );
+  }
+}
+
+export default App;
